44773095f0794e648e0805562ab30a4bd5b72a9a
sandyml <sandramayla@gmail.com>
2023-03-31 15:08:13 -0400

    Updated

---
+++ client/src/components/intro/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// TODO: Insert links for login and signup
+
+export const Landing = () => {
+ return (
+  <div>
+   <header className="Landing-header">
+    <p>
+     Please login or signup to access!
+    </p>
+     <h1>Landing</h1>
+   </header>
+  </div>
+ )
+}
---
+++ client/src/components/navigation/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+     <h2>navigation bar</h2>
+    </div>
+  )
+}
