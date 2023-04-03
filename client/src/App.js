bde637876133ac041ae9f0f2e89e319ccbfc1b3d
sandyml <sandramayla@gmail.com>
2023-04-03 17:58:31 -0400

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/intro/Home';
 import { Signup } from './components/authen/Signup';
 import { Login } from "./components/authen/Login";
 import { TermsPolicy } from './components/authen/TermsPolicy';
+import { HikeCarousel } from './components/intro/HikeCarousel';
 
 // TODO: If currentUser logged in show trails if not show login and signup to login 
 
@@ -13,6 +14,7 @@ export function App() {
   return (
     <Routes>
       <Navbar />
+      <HikeCarousel />
       <Route exact path="/home" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
---
+++ client/src/components/intro/HikeCarousel.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { ImageListItem } from '@material-ui/core';
+import trail_1 from '../../assets/trail_1.jpg'
+import trail_2 from '../../assets/trail_2.jpg'
+import trail_3 from '../../assets/trail_3.jpg'
+import trail_4 from '../../assets/trail_4.jpg'
+import trail_5 from '../../assets/trail_5.jpg'
+import trail_6 from '../../assets/trail_6.jpg'
+import trail_7 from '../../assets/trail_7.jpg'
+import trail_8 from '../../assets/trail_8.jpg'
+import trail_9 from '../../assets/trail_9.jpg'
+import trail_10 from '../../assets/trail_10.jpg'
+
+export function HikeCarousel() {
+
+    return (
+        <Carousel>
+            {itemData.map((item) => (
+                <ImageListItem key={item.id}>
+                    <img
+                        src={item.img}
+                        srcSet={item.img}
+                        alt={item.title}
+                        loading="lazy"
+                    />
+                </ImageListItem>
+            ))}
+        </Carousel>
+    )
+}
+
+const itemData = [
+    {
+        img: trail_1,
+    },
+
+    {
+        img: trail_2,
+    },
+
+    {
+        img: trail_3,
+    },
+
+    {
+        img: trail_4,
+    },
+
+    {
+        img: trail_5,
+    },
+
+    {
+        img: trail_6,
+    },
+
+    {
+        img: trail_7,
+    },
+    {
+        img: trail_8,
+    },
+
+    {
+        img: trail_9,
+    },
+
+    {
+        img: trail_10,
+    }
+
+]
