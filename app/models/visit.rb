e62bc7ef1e68a5b95380de6e452b34ac32192cb1
sandyml <sandramayla@gmail.com>
2023-04-07 16:12:17 -0400

    Updated

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,6 @@
 class Visit < ApplicationRecord
   validates :user_id, :trailhead_id, :visited_date, :visited, presence: true
+  # TODO: :visited_date might not be included / true if user have not visited hikes 
 
   belongs_to :user
   belongs_to :trailhead
