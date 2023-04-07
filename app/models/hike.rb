135cab38701331cba1896bc8ecd522d2c32cf612
sandyml <sandramayla@gmail.com>
2023-04-06 22:17:28 -0400

    Updated

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,8 @@ class Hike < ApplicationRecord
   validates :distance, :elevation_gain, :image_url, :trailhead_id, presence: true 
 
   belongs_to :trailhead
+ 
+  # TODO 
+  has_many :trailhead_amenities, dependent: :destroy
+  has_many :amenities, through: :trailhead_amenities
 end
--- app/serializers/trailhead_serializer.rb
+++ app/serializers/trailhead_serializer.rb
@@ -1,6 +1,14 @@
 class TrailheadSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :fees, :direction
+  attributes :id, :name, :location, :fees, :direction, :amenities
   # has_many :difficulties
+
+  has_many :hikes
+
+  # TODO: Can't grab amenities
+  def amenities
+    # byebug
+    object.hike.amenities.pluck(:name)
+  end
 
 end
 
