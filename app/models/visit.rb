54cd8ec499a76c7e2bc53a32dde2d5256d707f3f
sandyml <sandramayla@gmail.com>
2023-04-18 11:25:44 -0400

    Updated

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,18 +1,13 @@
 class Visit < ApplicationRecord
   validates :user_id, :trailhead_id, :visited_date, :visited, :record_visited_date_today, :record_visited_today, presence: true
-  # TODO: :visited_date might not be included / true if user have not visited hikes 
+  # validates :visited_date, presence: true
 
   belongs_to :user
   belongs_to :trailhead
-
-  # has_many :amenities
-  # delegate :amenities, to: :trailhead
-
-  validates :visited_date, presence: true
-
+  
   before_create :record_visited_date_today
   before_create :record_visited_today
-
+  
   def record_visited_date_today
     self.visited_date = Date.today
   end
@@ -20,5 +15,8 @@ class Visit < ApplicationRecord
   def record_visited_today
     self.visited = true
   end
-
+  
 end
+
+# has_many :amenities
+# delegate :amenities, to: :trailhead
